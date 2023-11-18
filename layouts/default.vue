7b29099e8c02459e86125a2172b272f61054d8cb
mmartofel <mmartofe@redhat.com>
2023-11-18 20:37:59 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,42 @@
+<template>
+    <UContainer>
+      <UCard class="mt-10">
+        <template #header>
+            <div class="flex justify-between">
+                <h1>Here we will be testing REST API integration with Nuxt 3 and Nuxt UI</h1>
+
+                <!-- Color scheme change button-->
+                <ClientOnly>
+                    <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+                                            color="gray"
+                                            variant="ghost"
+                                            aria-label="Theme"
+                                            @click="isDark = !isDark"
+                    />
+                    <template #fallback>
+                        <div class="w-8 h-8" />
+                    </template>
+                </ClientOnly>
+                <!-- end -->
+                
+            </div>
+        </template>
+            <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
+      </UCard>
+        <!-- Here page content will be injected -->
+        <slot />
+        <!-- end -->
+    </UContainer>
+</template>
+
+<script setup>
+    const colorMode = useColorMode()
+    const isDark = computed({
+    get () {
+            return colorMode.value === 'dark'
+    },
+    set () {
+        colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+    }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <UCard class="mt-10">
+        <div class="justify">
+            <span v-if="error"> Can't read data ... data table could not be displayed! {{ error }}</span>
+            <span v-else> 
+                <UTable :rows="people" /> 
+            </span>
+        </div>
+    </UCard>
+    <UCard class="mt-10">
+        <div>
+            <span v-if="error"> Data API perhaps failed my friend ...</span>
+            <span v-else>Data has being loaded.</span>
+            <span v-if="pending">Loading...</span>
+        </div>
+    </UCard>
+</template>
+
+<script setup lang="ts">
+    const url = "http://127.0.0.1:8080/api/person/all"
+    const { data: people, pending, error } = await useFetch(url)
+</script>
