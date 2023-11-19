a1aa53852e8910186c444f3e8fb579d0001be51a
mmartofel <mmartofe@redhat.com>
2023-11-19 17:35:15 +0100

    update Dockerfile #8

--- clean.sh
+++ clean.sh
@@ -2,3 +2,10 @@ oc delete imagestreamtag.image.openshift.io nuxt-ui-for-rest-api:latest
 oc delete buildconfig nuxt-ui-for-rest-api
 oc delete deployment nuxt-ui-for-rest-api
 oc delete service nuxt-ui-for-rest-api
+
+git add .
+git commit -m "update Dockerfile #8"
+git push origin main
+
+oc new-app https://github.com/mmartofel/nuxt-ui-for-rest-api
+
